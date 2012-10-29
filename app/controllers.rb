68e23c4145d0f645575c05ef474436dd523d7db9
Nat Welch <nat@natwelch.com>
2012-10-29 00:05:05 -0700

    messing around

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,22 +1,12 @@
 Scavenger.controllers  do
-  # get :index, :map => "/foo/bar" do
-  #   session[:foo] = "bar"
-  #   render 'index'
-  # end
+ get :index do
+   render 'index'
+ end
 
-  # get :sample, :map => "/sample/url", :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
+  get :level, :with => :id do
+    config = YAML.load_file('config.yml')
+    template = config["levels"][params[:id]]
 
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get "/example" do
-  #   "Hello world!"
-  # end
-
-  
+    render :"levels/#{template}"
+  end
 end
