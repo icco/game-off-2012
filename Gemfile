c6aa2c7cec89f584167ead9b91fb524019b8cb57
Nat Welch <nat@natwelch.com>
2012-10-30 23:10:00 -0700

    Adds basic auth at /login and /logout

--- Gemfile
+++ Gemfile
@@ -1,7 +1,10 @@
 source :rubygems
 
-gem 'activerecord', :require => "active_record"
-gem 'erubis', "~> 2.7.0"
+gem 'activerecord', :require => 'active_record'
+gem 'erubis', '~> 2.7.0'
+gem 'gravatar-ultimate' # https://github.com/sinisterchipmunk/gravatar
+gem 'json'
+gem 'omniauth-github' # https://github.com/intridea/omniauth-github
 gem 'padrino'
 gem 'rake'
 gem 'shotgun', :group => :developer
--- app/controllers.rb
+++ app/controllers.rb
@@ -6,9 +6,53 @@ Scavenger.controllers  do
   end
 
   get :level, :with => :id do
-    config = YAML.load_file('config.yml')
+    config = YAML.load_file("config.yml")
     template = config["levels"][params[:id]]
 
     render :"levels/#{template}"
+  end
+
+  get :login do
+    if Padrino.env == :development
+      redirect "/auth/developer"
+    else
+      redirect "/auth/github"
+    end
+  end
+
+  get :logout do
+    session.clear
+
+    redirect "/"
+  end
+
+  # For development testing
+  post "/auth/developer/callback" do
+    auth = request.env["omniauth.auth"]
+    auth = auth.info
+    logger.push(" Developer: #{auth.inspect}", :devel)
+
+    session[:username] = auth["name"]
+    session[:email] = auth["email"]
+
+    redirect "/"
+  end
+
+  # Github callback
+  get "/auth/github/callback" do
+    auth = request.env["omniauth.auth"]
+    auth = auth.info
+    logger.push(" Github: #{auth.inspect}", :devel)
+
+
+    session["email"] = auth["email"]
+    session["username"] = auth["nickname"]
+
+    redirect "/"
+  end
+
+  get "/auth/failure" do
+    flash[:notice] = params[:message]
+    redirect "/"
   end
 end
