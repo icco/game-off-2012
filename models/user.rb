ddc5ff860d760ae214173eb269f9ed2907bec24c
Nat Welch <nat@natwelch.com>
2012-11-08 21:07:58 -0800

    Render of first level works.

--- models/user.rb
+++ models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
   def get_completed
     return Progress.where(:user => self.name).to_a.map {|p| p.level }
   end
+
+  def self.get username
+    return User.find_or_create_by_name(username)
+  end
 end
